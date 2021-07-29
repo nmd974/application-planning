c75b58b842f6e16e688f14e3031361d15a5a7b7f
Gilles974 <boyer.gilles@live.fr>
2021-07-29 09:59:07 +0400

    modification table Role + mise a jour du model pour relation et factorie + seeder

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+       /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'label',
+    ];
+    public function users()
+    {
+        return $this->belongsTo('App\Models\User', 'role_id', 'id');
+    }
+
 }
