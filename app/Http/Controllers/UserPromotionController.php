c43eb99a03fabaa3a4248cf6c6977b484ac5636a
Gilles974 <boyer.gilles@live.fr>
2021-07-29 09:30:16 +0400

    mise en place des fichiers controler resource factorie migration model

---
+++ app/Http/Controllers/UserPromotionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User_promotion;
+use Illuminate\Http\Request;
+
+class UserPromotionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User_promotion  $user_promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User_promotion $user_promotion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User_promotion  $user_promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User_promotion $user_promotion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User_promotion  $user_promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User_promotion $user_promotion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User_promotion  $user_promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User_promotion $user_promotion)
+    {
+        //
+    }
+}
---
+++ database/factories/ExamPromotionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Exam_promotion;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExamPromotionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Exam_promotion::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
