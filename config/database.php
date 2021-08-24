5f7bf9e54a6f61fde570da03c2c1f79aca8a92ea
nmd974 <jha.payet@gmail.com>
2021-08-24 10:26:38 +0400

    ajout modif et delete exam activitie

--- config/database.php
+++ config/database.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-        // return $pdo;
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"],1);
+
 return [
 
     /*
@@ -50,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('CLEARDB_DATABASE_URL'),
-            'host' => $cleardb_server,
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $cleardb_db,
-            'username' => $cleardb_username,
-            'password' => $cleardb_password,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
