95ad509b22c12efbcb8310b6b86c10a6a9385a25
nmd974 <jha.payet@gmail.com>
2021-08-24 12:09:37 +0400

    suppression group scheme

--- routes/web.php
+++ routes/web.php
@@ -20,28 +20,26 @@ use App\Http\Controllers\UserPromotionController;
 |
 */
 
-Route::group(['scheme' => 'https'], function () {
-    Route::get('/', [PromotionController::class, 'index']);
-    Route::get('/{vue}', [PromotionController::class, 'index'])->name("getPromotions");
 
-    Route::get("/promotion/{id}", [PromotionController::class, 'dataPromotion'])->name("usersByPromotion");
-    Route::post('/promotion/create', [PromotionController::class, 'store'])->name("promotion.store");
-    Route::get('/promotion/archived/{id}', [PromotionController::class, 'destroy']);
-    Route::get('/promotion/{id}/exams', [PromotionController::class, 'dataPromotionExam'])->name("examsByPromotion");
+Route::get('/', [PromotionController::class, 'index']);
+Route::get('/{vue}', [PromotionController::class, 'index'])->name("getPromotions");
 
-    Route::post('/promotion/{id}/exams/create', [ExamController::class, 'store'])->name("createExamByPromotion");
-    Route::patch('/promotion/{promotion_id}/exams/{exam_id}/delete', [ExamPromotionController::class, 'destroy'])->name("deleteExamByPromotion");
+Route::get("/promotion/{id}", [PromotionController::class, 'dataPromotion'])->name("usersByPromotion");
+Route::post('/promotion/create', [PromotionController::class, 'store'])->name("promotion.store");
+Route::get('/promotion/archived/{id}', [PromotionController::class, 'destroy']);
+Route::get('/promotion/{id}/exams', [PromotionController::class, 'dataPromotionExam'])->name("examsByPromotion");
 
-    Route::post("/eleve/create", [UserController::class, 'store'])->name("users.store");
-    Route::patch("/eleve/update/{id}", [UserController::class, 'update'])->name("users.update");
+Route::post('/promotion/{id}/exams/create', [ExamController::class, 'store'])->name("createExamByPromotion");
+Route::patch('/promotion/{promotion_id}/exams/{exam_id}/delete', [ExamPromotionController::class, 'destroy'])->name("deleteExamByPromotion");
 
-    Route::patch("/eleve/{user_id}/promotion/{promotion_id}/delete", [UserPromotionController::class, 'destroy'])->name("user_promotions.destroy");
+Route::post("/eleve/create", [UserController::class, 'store'])->name("users.store");
+Route::patch("/eleve/update/{id}", [UserController::class, 'update'])->name("users.update");
 
-    Route::patch("/exam/update/{id}", [ExamController::class, 'update'])->name("updateExam");
+Route::patch("/eleve/{user_id}/promotion/{promotion_id}/delete", [UserPromotionController::class, 'destroy'])->name("user_promotions.destroy");
 
-    Route::get("/exam/{exam_id}/activities", [ExamActivitieController::class, 'index'])->name("getActivitiesByExam");
-    Route::post("/exam/{exam_id}/activities/create", [ActivitieController::class, 'store'])->name("createExamActivitie");
-    Route::patch("/exam/{exam_id}/activities/{activitie_id}/update", [ActivitieController::class, 'update'])->name("updateExamActivitie");
-    Route::patch("/exam/{exam_id}/activities/{activitie_id}/delete", [ExamActivitieController::class, 'destroy'])->name("deleteActivityByExam");
-});
+Route::patch("/exam/update/{id}", [ExamController::class, 'update'])->name("updateExam");
 
+Route::get("/exam/{exam_id}/activities", [ExamActivitieController::class, 'index'])->name("getActivitiesByExam");
+Route::post("/exam/{exam_id}/activities/create", [ActivitieController::class, 'store'])->name("createExamActivitie");
+Route::patch("/exam/{exam_id}/activities/{activitie_id}/update", [ActivitieController::class, 'update'])->name("updateExamActivitie");
+Route::patch("/exam/{exam_id}/activities/{activitie_id}/delete", [ExamActivitieController::class, 'destroy'])->name("deleteActivityByExam");
