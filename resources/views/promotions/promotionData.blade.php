73838c3f4d1b5aa6e772ae4468db07e7d6ea428e
nmd974 <jha.payet@gmail.com>
2021-08-25 10:15:22 +0400

    ajout du update de la promotion

--- resources/views/promotions/promotionData.blade.php
+++ resources/views/promotions/promotionData.blade.php
@@ -2,6 +2,7 @@
 @section('promotion')
 <div class="row mt-4">
     @foreach ( $promotions as $promotion)
+    @include('promotions.modal.update')
     <div class="col-xs-12 col-sm-11 col-md-3">
         <div class="card">
             <div class="card-header">
@@ -11,6 +12,9 @@
                 <a type="button" class="btn btn-outline-secondary"  href="/promotion/{{ $promotion->id }}">
                     <i class="fa fa-eye" aria-hidden="true"></i>
                 </a>
+                <button type="button" class="btn btn-success me-4" data-bs-toggle="modal" data-bs-target="#{{"edit_promotion_" . $promotion->id}}">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </button>
                 @if(!$promotion->archived)
                     <a type="button" class="btn btn-dark"  href="/promotion/archived/{{ $promotion->id }}">
                         <i class="fa fa-folder" aria-hidden="true"></i>
